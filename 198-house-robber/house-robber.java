134730e06e50fb811129d878610050035daafb76
vtu25501-jpg <vtu25501@veltech.edu.in>
2026-04-30 18:33:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (37.40%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int rob(int[] nums) {
+        int prev1 = 0, prev2 = 0;
+
+        for (int num : nums) {
+            int temp = Math.max(prev1, prev2 + num);
+            prev2 = prev1;
+            prev1 = temp;
+        }
+
+        return prev1;
+    }
+}
