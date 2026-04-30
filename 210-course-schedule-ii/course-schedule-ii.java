69d213b1b313cc675f3fe045277c79af0b53d51e
vtu25501-jpg <vtu25501@veltech.edu.in>
2026-04-30 18:18:15 +0530

    Time: 5 ms (83.95%) | Memory: 46.6 MB (82.10%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] p : prerequisites) {
+            graph.get(p[1]).add(p[0]);
+            indegree[p[0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        int[] result = new int[numCourses];
+        int index = 0;
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            result[index++] = curr;
+
+            for (int nei : graph.get(curr)) {
+                indegree[nei]--;
+                if (indegree[nei] == 0) {
+                    queue.add(nei);
+                }
+            }
+        }
+
+        return index == numCourses ? result : new int[0];
+    }
+}
