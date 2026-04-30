d7a47c5970c110b9181505662473c242f40d15ce
vtu25501-jpg <vtu25501@veltech.edu.in>
2026-04-30 18:29:56 +0530

    Time: 3 ms (60.56%) | Memory: 44 MB (47.86%) - LeetSync

---
+++ 1873-longest-nice-substring/longest-nice-substring.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+    public String longestNiceSubstring(String s) {
+        if (s.length() < 2) return "";
+
+        Set<Character> set = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            set.add(c);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (set.contains(Character.toLowerCase(c)) && set.contains(Character.toUpperCase(c))) {
+                continue;
+            }
+
+            String left = longestNiceSubstring(s.substring(0, i));
+            String right = longestNiceSubstring(s.substring(i + 1));
+
+            return left.length() >= right.length() ? left : right;
+        }
+
+        return s;
+    }
+}
