158b29399890e41d595719b32b0c51f6dda77d80
vtu25501-jpg <vtu25501@veltech.edu.in>
2026-04-30 18:10:56 +0530

    Time: 124 ms (58.21%) | Memory: 279.1 MB (74.14%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        boolean[] visited = new boolean[n];
+        return dfs(graph, visited, source, destination);
+    }
+
+    private boolean dfs(List<List<Integer>> graph, boolean[] visited, int curr, int dest) {
+        if (curr == dest) return true;
+
+        visited[curr] = true;
+
+        for (int nei : graph.get(curr)) {
+            if (!visited[nei]) {
+                if (dfs(graph, visited, nei, dest)) return true;
+            }
+        }
+
+        return false;
+    }
+}
